cf43b9a957ea5e4602f25afcdc845a66a965e561
Vinson Cernuto <cernutovinson@gmail.com>
2020-10-30 16:03:09 -0400

    hmtlRoutes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+// Dependencies
+
+var path = require('path');
+
+module.exports = function(app){
+    // Called for continue workout and new work out is clicked 
+    app.get("/exercises", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+
+    app.get("/status", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/status.html"));
+    });
+};
